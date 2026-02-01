7a951e6caf19a136c114b837c4c26fb45fb02e36
Patrickli18 <iittiljw@gmail.com>
2026-02-02 07:20:28 +0900

    update

---
+++ day02/quotation_manipulation.py
@@ -0,0 +1,24 @@
+#Quotation Manipulation
+# Character printing methods in Python
+
+# IBM Thomas Watson 
+quote = "I think there is a world market for maybe five computers."
+print("Original quote:" )
+print(quote)
+
+print("\nIn uppercase:")
+print(quote.upper())
+
+print("\nIn lowercase:")
+print(quote.lower())
+
+print("\nAs a title:" )
+print(quote.title())
+
+print("\nWith amiinor replacement:" )
+print(quote.replace("computers", "typewriters"))
+
+print("\nOriginal quote is still:")
+print(quote)
+
+input("\n\nPress Enter to exit.")
