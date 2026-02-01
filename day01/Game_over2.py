ccc57a7dcfc053baa5ff9f17a2e76ae56d8a0aba
Patrickli18 <iittiljw@gmail.com>
2026-02-02 07:09:59 +0900

    update

---
+++ day01/Game_over2.py
@@ -0,0 +1,21 @@
+#Game Over -version 2
+
+print("Program 'Game Over' 2.0")
+print("Same","message","as before")
+
+print("Just",
+      "a bit",
+      "bigger")
+
+print("Here", end=" ")
+print("it is... ")
+
+print(
+    """
+    ########################################################
+      Game Over
+    ########################################################
+""")
+#input("\n \n Press Enter to exit.")
+
+print(100//3)
