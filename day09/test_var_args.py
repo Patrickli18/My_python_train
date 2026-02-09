cc7de33c4a7307d12b51bb16b472b4aaed4666eb
Patrickli18 <iittiljw@gmail.com>
2026-02-09 09:15:54 +0900

    update

--- day09/test_var_args.py
+++ day09/test_var_args.py
@@ -13,6 +13,18 @@ print("#"*10)
 def get_me(**kwargs):
     for key,value in kwargs.items():
         print("{0} == {1}".format(key,value))
-    
 
 get_me(name = "yasoob")
+
+print("#"*10)
+
+
+def test_args_kwargs(arg1,arg2,arg3):
+    print("arg1:", arg1)
+    print("arg2:", arg2)
+    print("arg3:", arg3)
+
+
+arg = ("two", 3 ,5)
+
+test_args_kwargs(*arg)
