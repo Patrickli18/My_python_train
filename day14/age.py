3eab4371251da7f839700c9c807038fc8bede8e3
Patrickli18 <iittiljw@gmail.com>
2026-02-14 08:40:03 +0900

    update

---
+++ day14/age.py
@@ -0,0 +1,21 @@
+# Name and Age
+
+import random
+class Foo:
+    def __init__(self, name):
+        self.name = name
+    def get_name(self, age):
+        if self.select(age):
+            return self.name
+        else:
+            return "the name is secret."
+    @staticmethod
+    def select(n):
+        a = random.randint(1,100)
+        return a - n > 0
+    
+if __name__ == "__main__":
+    f = Foo("luolaoshi")
+    name = f.get_name(22)
+    print(name)
+    
